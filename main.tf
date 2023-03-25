aaac748f6e5c9d9a0ab44c8241b17cfc08431c01
Bis Tripathy <43734679+justb1swa@users.noreply.github.com>
2023-03-25 15:52:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,10 +15,6 @@ resource "aws_instance" "staging" {
   #user_data                   = "${data.template_file.provision.rendered}"
   #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
   #Using the Key Pair wile Launching the Instance
-resource "aws_key_pair" "developer" {
-  key_name   = "developer"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0zHZqeCyz/W86cnaAhHKEuN8XuhDDFwmIq7Bw1YLfa9lCdNDVdMneg5+01hCODh7PHIE2vqdhzuX2Iu4qjzlEDPy4qK+5LCAzEInFTOYdkJfqwV255UHAUz69u9Mi+sdhIYPqYPMBkG8mNGhm3cv6XDNRYhQ9+Dj63K4hpXPzJczuzZQhAIv7/v6bGkMNmYtxzJc3mlDDSIaYqzMIcdnJwKzAgyfUU8deuMGmj3PNeF6zpnRmgyuFxNkU2X1QD+fsmmLAkaQK43NlQBLZ8JFm8guErU69NVuTgAXZUZnbjZaBm3wBe1Ay7qsu4dm4vfJem2CyuFU1qMBBEdx06QJB biskeypairawscmu"
-#public_key = "${file("developer.pub")}
 }
   lifecycle {
     create_before_destroy = true
@@ -26,7 +22,10 @@ resource "aws_key_pair" "developer" {
   tags = {
     "Name" = "staging"
   }
-  
+resource "aws_key_pair" "developer" {
+  key_name   = "developer"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0zHZqeCyz/W86cnaAhHKEuN8XuhDDFwmIq7Bw1YLfa9lCdNDVdMneg5+01hCODh7PHIE2vqdhzuX2Iu4qjzlEDPy4qK+5LCAzEInFTOYdkJfqwV255UHAUz69u9Mi+sdhIYPqYPMBkG8mNGhm3cv6XDNRYhQ9+Dj63K4hpXPzJczuzZQhAIv7/v6bGkMNmYtxzJc3mlDDSIaYqzMIcdnJwKzAgyfUU8deuMGmj3PNeF6zpnRmgyuFxNkU2X1QD+fsmmLAkaQK43NlQBLZ8JFm8guErU69NVuTgAXZUZnbjZaBm3wBe1Ay7qsu4dm4vfJem2CyuFU1qMBBEdx06QJB biskeypairawscmu"
+#public_key = "${file("developer.pub")}  
  
 }
 #Create a new EC2 launch configuration
@@ -40,10 +39,6 @@ resource "aws_instance" "prod" {
   #user_data                   = "${data.template_file.provision.rendered}"
   #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
   #Using the Key Pair wile Launching the Instance
-resource "aws_key_pair" "developer" {
-  key_name   = "developer"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC0zHZqeCyz/W86cnaAhHKEuN8XuhDDFwmIq7Bw1YLfa9lCdNDVdMneg5+01hCODh7PHIE2vqdhzuX2Iu4qjzlEDPy4qK+5LCAzEInFTOYdkJfqwV255UHAUz69u9Mi+sdhIYPqYPMBkG8mNGhm3cv6XDNRYhQ9+Dj63K4hpXPzJczuzZQhAIv7/v6bGkMNmYtxzJc3mlDDSIaYqzMIcdnJwKzAgyfUU8deuMGmj3PNeF6zpnRmgyuFxNkU2X1QD+fsmmLAkaQK43NlQBLZ8JFm8guErU69NVuTgAXZUZnbjZaBm3wBe1Ay7qsu4dm4vfJem2CyuFU1qMBBEdx06QJB biskeypairawscmu"
-#public_key = "${file("developer.pub")}
 }
   lifecycle {
     create_before_destroy = true
